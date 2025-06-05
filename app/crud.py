59a25a7b67b1da56056799b6be23c8239c695cb1
JosefCAD42 <jwanschura@cad42.com>
2025-06-05 14:45:59 +0200

    fix: ajout import Dict pour SensorHistoryResponse

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from app import models, schemas, auth
+from typing import Optional
 
 def create_sensor(db: Session, sensor: schemas.SensorCreate, user_id: str):
     db_sensor = models.Sensor(**sensor.dict(), created_by=user_id)
