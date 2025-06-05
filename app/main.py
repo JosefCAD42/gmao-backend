83c100f8f54057be77e78fe1ba244227ae907b6a
JosefCAD42 <jwanschura@cad42.com>
2025-06-05 16:34:52 +0200

    Fix register route: added registration_key and logging

--- app/main.py
+++ app/main.py
@@ -5,9 +5,17 @@ from app.routers import sensors
 from app.routers import checklists
 from app.routers import users
 from app.routers import dashboard
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Remplace "*" par l'URL exacte de ton frontend si tu veux plus de sécurité
+    allow_credentials=True,
+    allow_methods=["*"],  # ⬅️ très important pour autoriser les OPTIONS, POST, etc.
+    allow_headers=["*"],  # ⬅️ permet les headers comme Authorization
+)
 # 👇 Crée toutes les tables à partir des modèles
 Base.metadata.create_all(bind=engine)
 
